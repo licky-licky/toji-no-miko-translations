3a07d364ce67ce1c46f134159f69e09fa37f1068
swordpolishing <papikamail@gmail.com>
2021-08-07 20:15:39 +0200

    add: generate list of character names and their appearance counts

--- utils/event_characters.py
+++ utils/event_characters.py
@@ -1,20 +1,25 @@
 import json, os, re
 from glob import glob
 from pathlib import Path
+from collections import Counter
 
 file_dir = os.path.abspath(__file__)
 ROOT_DIR = Path(file_dir).parent.parent.absolute()
 
 def main() -> str:
-    output = []
-
+    events = []
+    characters = {}
     for f in glob(str(ROOT_DIR) + '/docs/*.md'):
         with open(f, 'r') as event_file:
             event_text =  ''.join(event_file.readlines())
             result = re.findall(r'((?<=\*\*)[A-Z]\w+:(?=\*\*))', event_text)
             appearing_characters = list(set([char.strip(':') for char in result]))
-            output.append({ Path(f).stem : appearing_characters})
-
+            events.append({ 'name': Path(f).stem, 'chars' : appearing_characters})
+    for event in events:
+        chars = event['chars']
+        characters =  {k: characters.get(k, 0) + dict(Counter(chars)).get(k, 0) for k in characters | dict(Counter(chars))}
+    characters = dict(sorted(characters.items(), key=lambda item: item[1], reverse=True))
+    output = { 'events': events, 'characters': characters}
     return json.dumps(output)
 
 if __name__ == '__main__':
