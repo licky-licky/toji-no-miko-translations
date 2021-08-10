3a3c98c7d0e8c74b88af7efe6bd12bb3e81e7dd1
swordpolishing <papikamail@gmail.com>
2021-08-10 21:20:19 +0200

    fix: filter unneeded characters from appearance list

--- utils/event_characters.py
+++ utils/event_characters.py
@@ -4,22 +4,30 @@ from pathlib import Path
 from collections import Counter
 
 file_dir = os.path.abspath(__file__)
-ROOT_DIR = Path(file_dir).parent.parent.absolute()
+root_dir = Path(file_dir).parent.parent.absolute()
+filter_chars = ['Announcement', 'Shopkeeper', 'Everyone', 'Announcer',
+                'Students', 'Both', 'Child', 'Angtelina', 'Spectators',
+                'Scream', 'Dealer', 'Landlady', 'NAnoha', 'Suuu', 'Itsuski',
+                'Yunna', 'Operator', 'Attendant', 'Organizer', 'Detective', 'Priest',
+                'Heroes', 'Culpit', 'Weatherman', 'Karou', 'Honoko', 'Voice', 'Other', 'Tojis',
+                'Children', 'Police', 'Culprit', 'Referee', 'Aradama']
 
 def main() -> str:
     events = []
     characters = {}
-    for f in glob(str(ROOT_DIR) + '/docs/*.md'):
+    is_a_real_character = lambda x: x not in filter_chars
+    for f in glob(str(root_dir) + '/docs/*.md'):
         with open(f, 'r') as event_file:
             event_text =  ''.join(event_file.readlines())
             result = re.findall(r'((?<=\*\*)[A-Z]\w+:(?=\*\*))', event_text)
             appearing_characters = list(set([char.strip(':') for char in result]))
+            appearing_characters = list(filter(is_a_real_character, appearing_characters))
             events.append({ 'name': Path(f).stem, 'chars' : appearing_characters})
     for event in events:
         chars = event['chars']
         characters =  {k: characters.get(k, 0) + dict(Counter(chars)).get(k, 0) for k in characters | dict(Counter(chars))}
     characters = dict(sorted(characters.items(), key=lambda item: item[1], reverse=True))
-    output = { 'events': events, 'characters': characters}
+    output = { 'events': events, 'characters': characters }
     return json.dumps(output)
 
 if __name__ == '__main__':
