73615b54e7cd4a88c89ba43cb41f0ab78f784171
swordpolishing <papikamail@gmail.com>
2021-08-31 22:18:27 +0200

    remove codedoc files

--- .old/.codedoc/components/iframe/style.ts
+++
@@ -1,9 +0,0 @@
-import { themedStyle } from '@connectv/jss-theme';
-import { CodedocTheme } from '@codedoc/core';
-import { theme } from '../../theme';
-
-export const TagStyle = themedStyle<CodedocTheme>(theme => ({
-    tag: {
-        color: 'blue'
-    }
-}))
